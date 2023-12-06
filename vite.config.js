4df2a4c9a0e478536aa3c4074542dacba02abf1f
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-12-06 14:09:56 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  build: {
+    manifest: true, // Generates a manifest file
+    outDir: 'dist', // Specify the output directory
+    assetsDir: 'assets', // Specify the assets directory
+    cssCodeSplit: true, // Enable CSS code splitting
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Customize the entry file names
+        chunkFileNames: '[name].js', // Customize the chunk file names
+        assetFileNames: 'assets/[name].[ext]', // Customize the name of the emitted files
+      },
+    },
+    assetFileNames: 'assets/[name].[ext]', // Customize the name of the emitted files
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
